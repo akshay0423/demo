031a1c6563cb9ac8c71786fff735221e9cc477cc
Akshay <akshay0423@yahoo.com>
2025-04-10 14:07:26 +0530

    add subtraction

--- src/main/java/org/example/CalculatorApp.java
+++ src/main/java/org/example/CalculatorApp.java
@@ -4,6 +4,9 @@ public class CalculatorApp {
     public static int add(int a, int b) {
         return a + b;
     }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 
     public static void main(String[] args) {
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.example.CalculatorApp.add;
+import static org.example.CalculatorApp.subtract;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
@@ -8,5 +9,10 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         assertEquals(36, add(23, 13));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(36, subtract(23, 13));
     }
 }
